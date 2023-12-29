d40f403ed6b009262c6a119c51e164e34342fec8
ATOUI Abderahman Yakoub <117508580+ATOUIYakoub@users.noreply.github.com>
2023-12-29 11:20:23 +0100

    fix workingdays choice and workinghours

--- backend/lawyers/serializers.py
+++ backend/lawyers/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Lawyer
 
 class LawyerSerializer(serializers.ModelSerializer):
+    workingDays = serializers.ListField(
+        child=serializers.CharField(max_length=50, allow_blank=True),
+        allow_empty=True
+    )
+    
+    workingHours = serializers.ListField(
+        child=serializers.CharField(max_length=50, allow_blank=True),
+        allow_empty=True
+    )
     class Meta:
         model = Lawyer
         fields = '__all__'
